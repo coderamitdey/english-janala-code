7969b4ba1f5841031a1b5cd98c9a4bae1b176841
coderamitdey <amitdey7150@gmail.com>
2025-09-07 12:31:29 +0600

    header and banner section works done

---
+++ js/index.js
@@ -0,0 +1,23 @@
+const loadLessons = ()=>{
+    fetch("https://openapi.programming-hero.com/api/levels/all")
+    .then((res)=> res.json())
+    .then((json) => displayLesson(json.data));
+};
+
+const displayLesson=(lessons)=>{
+    //  1. get the container
+ const levelContainer = document.getElementById('level-container');
+ levelContainer.innerHTML= '';
+    // 2. get into every lessons
+ for(let lesson of lessons){
+
+    // 3.create element
+ const btnDiv = document.createElement('div');
+ btnDiv.innerHTML = `
+        <button class="btn btn-outline btn-primary"><i class="fa-solid fa-book"></i>Lesson ${lesson.level_no}</button>`;
+
+    // 4. append into container
+    levelContainer.append(btnDiv)
+ }
+};
+loadLessons();
